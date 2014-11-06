277cb6f4db690fb15a9dddafe3d7d227f2d0a66d
Jack O'Connor <oconnor663@gmail.com>
2014-11-06 10:46:11 -0800

    diagonal lines

--- ttt.py
+++ ttt.py
@@ -29,8 +29,34 @@ def get_victory_lines(board):
     # And all the columns.
     for col in range(get_width(board)):
         lines.append([(row, col) for row in range(get_height(board))])
-    # At some point figure out diagonals.
+    lines.extend(get_diagonal_victory_lines(board))
     return lines
+
+
+def get_diagonal_victory_lines(board):
+    # We want this to work on non-square boards too! There are several ways we
+    # could define it. To keep things simple, I want all diagonal lines to go
+    # at 45 degrees, rather than trying to figure out weird slants. So in a
+    # non-square board, the diagonals won't actually go from corner to corner.
+    # We'll still say they have to *start* in a corner, again to keep things
+    # simple.
+    height = get_height(board)
+    width = get_width(board)
+    diag_len = min(height, width)
+    # Note that we double count diagonals for square boards. Thats ok.
+    return [[(i, i) for i in range(diag_len)],
+            [(height - 1 - i, i) for i in range(diag_len)],
+            [(i, width - 1 - i) for i in range(diag_len)],
+            [(height - 1 - i, width - 1 - i) for i in range(diag_len)]]
+
+
+def debug_print_victory_lines(height, width):
+    for line in get_victory_lines(new_board(height, width)):
+        dummy_board = new_board(height, width)
+        for row, col in line:
+            dummy_board[row][col] = 'x'
+        print_board(dummy_board)
+        print()
 
 
 def get_winner(board):
